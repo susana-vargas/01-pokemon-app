dabc5d31359e99e8082acbaa69d980cc490cf72c
susana-vargas <hernandeznerisusanaisabel@gmail.com>
2025-11-20 21:09:37 -0600

    test: add unit test to verify service call with correct parameters in PokemonsController

--- src/pokemons/pokemons.controller.spec.ts
+++ src/pokemons/pokemons.controller.spec.ts
@@ -1,9 +1,11 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { PokemonsController } from './pokemons.controller';
 import { PokemonsService } from './pokemons.service';
+import { PaginationDto } from 'src/shared/dtos/pagination.dto';
 
 describe('PokemonsController', () => {
   let controller: PokemonsController;
+  let service: PokemonsService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -12,9 +14,27 @@ describe('PokemonsController', () => {
     }).compile();
 
     controller = module.get<PokemonsController>(PokemonsController);
+    service = module.get<PokemonsService>(PokemonsService);
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  it('should have called the service with correct parameter', async () => {
+    const dto: PaginationDto = { limit: 10, page: 1 };
+
+    //crea un espia, estara pendiente del servisio para saber si hay algun cambio
+    jest.spyOn(service, 'findAll');
+
+    await controller.findAll(dto);
+
+    //si se ejecuto el metodo
+    // eslint-disable-next-line @typescript-eslint/unbound-method
+    expect(service.findAll).toHaveBeenCalled();
+
+    //Y que lo ayan llamado con este objeto exacto
+    // eslint-disable-next-line @typescript-eslint/unbound-method
+    expect(service.findAll).toHaveBeenCalledWith(dto);
   });
 });
