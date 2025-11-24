4ad9539f6e0f6a08349a773b513cd527d5c5995f
susana-vargas <hernandeznerisusanaisabel@gmail.com>
2025-11-23 22:57:09 -0600

    feat: add unit tests for application bootstrap and export bootstrap function

---
+++ src/main.spec.ts
@@ -0,0 +1,37 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { bootstrap } from './main';
+
+jest.mock('@nestjs/core', () => ({
+  NestFactory: {
+    create: jest.fn().mockResolvedValue({
+      useGlobalPipes: jest.fn(),
+      setGlobalPrefix: jest.fn(),
+      listen: jest.fn(),
+    }),
+  },
+}));
+
+describe('Main.ts Bootstrap', () => {
+  let mockApp: {
+    useGlobalPipes: jest.Mock;
+    setGlobalPrefix: jest.Mock;
+    listen: jest.Mock;
+  };
+
+  beforeEach(() => {
+    mockApp = {
+      useGlobalPipes: jest.fn(),
+      setGlobalPrefix: jest.fn(),
+      listen: jest.fn(),
+    };
+
+    (NestFactory.create as jest.Mock).mockResolvedValue(mockApp);
+  });
+
+  it('should create application', async () => {
+    await bootstrap();
+    // eslint-disable-next-line @typescript-eslint/unbound-method
+    expect(NestFactory.create).toHaveBeenCalledWith(AppModule);
+  });
+});
