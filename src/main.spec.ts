46b91dcf3e1f73cf09972c7d6b963cf38340acc6
susana-vargas <hernandeznerisusanaisabel@gmail.com>
2025-11-24 22:28:11 -0600

    refactor: improve comments for clarity in main.spec.ts

--- src/main.spec.ts
+++ src/main.spec.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { bootstrap } from './main';
 
+//se pone como dato mock para simular el comportamiento de la app levantada
 jest.mock('@nestjs/core', () => ({
   NestFactory: {
     create: jest.fn().mockResolvedValue({
@@ -19,13 +20,16 @@ describe('Main.ts Bootstrap', () => {
     listen: jest.Mock;
   };
 
+  //indica que se ejecuta antes de cada prueba unitaria
   beforeEach(() => {
+    //se inicializa la aplicacion
     mockApp = {
       useGlobalPipes: jest.fn(),
       setGlobalPrefix: jest.fn(),
       listen: jest.fn(),
     };
 
+    //se configura el mock para que devuelva la aplicacion simulada
     (NestFactory.create as jest.Mock).mockResolvedValue(mockApp);
   });
 
@@ -33,5 +37,26 @@ describe('Main.ts Bootstrap', () => {
     await bootstrap();
     // eslint-disable-next-line @typescript-eslint/unbound-method
     expect(NestFactory.create).toHaveBeenCalledWith(AppModule);
+  });
+
+  it('should set global prefix', async () => {
+    await bootstrap();
+    //se prueba el prefix de la url 'api/pokemon'
+    expect(mockApp.setGlobalPrefix).toHaveBeenCalledWith('api');
+  });
+
+  it('should listen on port 3000 if env port not set', async () => {
+    await bootstrap();
+    //espera a que cuando la app se levante sea en el puerto 3000
+    expect(mockApp.listen).toHaveBeenCalledWith(3000);
+  });
+
+  //que si se configura el puerto que use ese puerto
+  it('should listen on env port', async () => {
+    process.env.PORT = '4200';
+
+    await bootstrap();
+
+    expect(mockApp.listen).toHaveBeenCalledWith(process.env.PORT);
   });
 });
