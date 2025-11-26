c7efb454e975f95dcc5119638e7e23f0123d7a4f
susana-vargas <hernandeznerisusanaisabel@gmail.com>
2025-11-25 22:17:01 -0600

    feat: solucion de la tarea - parte 2

--- src/pokemons/pokemons.service.spec.ts
+++ src/pokemons/pokemons.service.spec.ts
@@ -90,6 +90,16 @@ describe('PokemonsService', () => {
     );
   });
 
+  it('should return a pokemon from cache', async () => {
+    const cacheSpy = jest.spyOn(service.pokemonsCache, 'get');
+    const id = 1;
+
+    await service.findOne(id);
+    await service.findOne(id);
+
+    expect(cacheSpy).toHaveBeenCalledTimes(1);
+  });
+
   it('should find all pokemons and cache them', async () => {
     const pokemons = await service.findAll({ limit: 10, page: 1 });
 
@@ -98,5 +108,18 @@ describe('PokemonsService', () => {
 
     expect(service.paginatedPokemonsCache.has('10-1')).toBeTruthy();
     expect(service.paginatedPokemonsCache.get('10-1')).toBe(pokemons);
+  });
+
+  it('should return pokemons from cache', async () => {
+    const cacheSpy = jest.spyOn(service.paginatedPokemonsCache, 'get');
+    const fetchSpy = jest.spyOn(global, 'fetch');
+
+    await service.findAll({ limit: 10, page: 1 });
+    await service.findAll({ limit: 10, page: 1 });
+
+    expect(cacheSpy).toHaveBeenCalledTimes(1);
+    expect(cacheSpy).toHaveBeenCalledWith('10-1');
+
+    expect(fetchSpy).toHaveBeenCalledTimes(11);
   });
 });
