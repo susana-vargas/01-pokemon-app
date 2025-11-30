94d52359c26dce4b772eb098e27b2f8fd350c6b1
susana-vargas <hernandeznerisusanaisabel@gmail.com>
2025-11-29 19:51:47 -0600

    feat: e2e - post: crear pokemon

---
+++ test/e2e/pokemons/pokemons.e2e-spec.ts
@@ -0,0 +1,33 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { INestApplication, ValidationPipe } from '@nestjs/common';
+import request from 'supertest';
+import { App } from 'supertest/types';
+import { AppModule } from '../../../src/app.module';
+
+describe('Pokemons (e2e)', () => {
+  let app: INestApplication<App>;
+
+  beforeEach(async () => {
+    const moduleFixture: TestingModule = await Test.createTestingModule({
+      imports: [AppModule],
+    }).compile();
+
+    app = moduleFixture.createNestApplication();
+
+    app.useGlobalPipes(
+      new ValidationPipe({
+        whitelist: true,
+        forbidNonWhitelisted: true,
+      }),
+    );
+
+    await app.init();
+  });
+
+  it('/pokemons (POST) - with no body', () => {
+    return request(app.getHttpServer())
+      .post('/pokemons')
+      .expect(200)
+      .expect('Hello World!');
+  });
+});
