34836e2bfa50f90478007e51707acfee8ea92906
susana-vargas <hernandeznerisusanaisabel@gmail.com>
2025-11-29 20:26:02 -0600

    feat: e2e - post: con y sin body

--- test/e2e/pokemons/pokemons.e2e-spec.ts
+++ test/e2e/pokemons/pokemons.e2e-spec.ts
@@ -24,10 +24,43 @@ describe('Pokemons (e2e)', () => {
     await app.init();
   });
 
-  it('/pokemons (POST) - with no body', () => {
-    return request(app.getHttpServer())
-      .post('/pokemons')
-      .expect(200)
-      .expect('Hello World!');
+  it('/pokemons (POST) - with no body ', async () => {
+    const response = await request(app.getHttpServer()).post('/pokemons');
+    //verifica si viene si no es un array vacio
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-member-access
+    const messageArray = response.body.message ?? [];
+
+    expect(response.statusCode).toBe(400);
+    //se verifica si el mensaje de error contiene los mensajes esperados
+    expect(messageArray).toContain('name must be a string');
+    expect(messageArray).toContain('name should not be empty');
+    expect(messageArray).toContain('type must be a string');
+    expect(messageArray).toContain('type should not be empty');
+
+    // return request(app.getHttpServer())
+    //   .post('/pokemons')
+    //   .expect(200)
+    //   .expect('Hello World!!');
+  });
+
+  it('/pokemons (POST) - with no body 2', async () => {
+    //se verifica lo mismo pero mas limpio
+    const response = await request(app.getHttpServer()).post('/pokemons');
+
+    //no importa si vienen en un orden diferente
+    const mostHaveErrorMessage = [
+      'type should not be empty',
+      'name must be a string',
+      'type must be a string',
+      'name should not be empty',
+    ];
+
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-member-access
+    const messageArray: string[] = response.body.message ?? [];
+
+    //se verifica la cantidadd de mensajes de error
+    expect(mostHaveErrorMessage.length).toBe(messageArray.length);
+    //verifica que los errores sean los esperados
+    expect(messageArray).toEqual(expect.arrayContaining(mostHaveErrorMessage));
   });
 });
